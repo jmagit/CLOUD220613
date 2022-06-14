9742c975bdefe6c07a0616c768b1a622e72e9c50
Javier <jmartin@grupoloyal.com>
2022-06-14 18:02:38 +0200

    Soluciones

--- demoIEEE.js
+++ demoIEEE.js
@@ -3,3 +3,15 @@ console.log((0.1 + 0.2))
 console.log((1.0 - 0.9) === 0.1)
 console.log((1.0 - 0.9))
 console.log(1 / 0)
+
+// impresora = 4;
+// if(inpresora == 4) {
+//     console.log("imprime")
+// }
+
+a = 1
+b = "2$"
+
+c = parseInt(b) * a
+
+console.log(c)
